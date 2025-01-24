d21b746fa6beda1ba47df2f90aa9e276f97a909a
jsep <jomchai.suk09@gmail.com>
2025-01-24 20:03:45 +0700

    "Fix Form control.tsx "

--- App/src/components/Control.tsx
+++ App/src/components/Control.tsx
@@ -1,113 +1,115 @@
 const Control = () => {
   return (
-    <form className="flex justify-between mb-5">
-      <div className="flex">
-        <label
-          htmlFor="search-dropdown"
-          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-        >
-          Your Email
-        </label>
-        <button
-          id="dropdown-button"
-          data-dropdown-toggle="dropdown"
-          className="shrink-0 z-10 inline-flex items-center  px-1 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 "
-          type="button"
-        >
-          All categories{" "}
-          <svg
-            className="w-2.5 h-2.5 ms-2.5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 6"
+    <div className="flex justify-between mb-5">
+      <form>
+        <div className="flex">
+          <label
+            htmlFor="search-dropdown"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
           >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 1 4 4 4-4"
-            />
-          </svg>
-        </button>
-        <div
-          id="dropdown"
-          className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-20"
-        >
-          <ul
-            className="py-2 text-sm text-gray-700 dark:text-gray-200"
-            aria-labelledby="dropdown-button"
-          >
-            <li>
-              <button
-                type="button"
-                className="inline-flex w-full px-4 py-2 hover:bg-gray-100"
-              >
-                Mockups
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="inline-flex w-full px-4 py-2 hover:bg-gray-100"
-              >
-                Templates
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="inline-flex w-full px-4 py-2 hover:bg-gray-100"
-              >
-                Design
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="inline-flex w-full px-4 py-2 hover:bg-gray-100"
-              >
-                Logos
-              </button>
-            </li>
-          </ul>
-        </div>
-        <div className="relative w-full">
-          <input
-            type="search"
-            id="search-dropdown"
-            className="block p-2.5 min-w-96 z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300"
-            placeholder="Search Mockups, Logos, Design Templates..."
-            required
-          />
+            Your Email
+          </label>
           <button
-            type="submit"
-            className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg "
+            id="dropdown-button"
+            data-dropdown-toggle="dropdown"
+            className="shrink-0 z-10 inline-flex items-center  px-1 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 "
+            type="button"
           >
+            All categories{" "}
             <svg
-              className="w-4 h-4"
+              className="w-2.5 h-2.5 ms-2.5"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              viewBox="0 0 20 20"
+              viewBox="0 0 10 6"
             >
               <path
                 stroke="currentColor"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                d="m1 1 4 4 4-4"
               />
             </svg>
-            <span className="sr-only">Search</span>
           </button>
+          <div
+            id="dropdown"
+            className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-20"
+          >
+            <ul
+              className="py-2 text-sm text-gray-700 dark:text-gray-200"
+              aria-labelledby="dropdown-button"
+            >
+              <li>
+                <button
+                  type="button"
+                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100"
+                >
+                  Mockups
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100"
+                >
+                  Templates
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100"
+                >
+                  Design
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  className="inline-flex w-full px-4 py-2 hover:bg-gray-100"
+                >
+                  Logos
+                </button>
+              </li>
+            </ul>
+          </div>
+          <div className="relative w-full">
+            <input
+              type="search"
+              id="search-dropdown"
+              className="block p-2.5 min-w-96 z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300"
+              placeholder="Search..."
+              required
+            />
+            <button
+              type="submit"
+              className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg "
+            >
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+              <span className="sr-only">Search</span>
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
       <button className="bg-blue-600 text-white p-2 rounded-md hover:bg-blue-500 cursor-pointer">
         Add New Student
       </button>
-    </form>
+    </div>
   );
 };
 
