d5903c67f3a3224de4c29fec0c30f282ab2cd8e3
jsep <jomchai.suk09@gmail.com>
2025-01-25 10:18:47 +0700

    Add className StudentList

--- App/src/pages/StudentsList.tsx
+++ App/src/pages/StudentsList.tsx
@@ -2,7 +2,7 @@ import Control from "../components/Control";
 import StudentTable from "../components/StudentTable";
 const StudentsList = () => {
   return (
-    <div>
+    <div className="container min-h-screen max-w-7xl m-auto p-15">
       <header>
         <div className="flex justify-between items-center">
           <p className="text-4xl">Student Details</p>
