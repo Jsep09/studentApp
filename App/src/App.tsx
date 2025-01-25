7cbb9f3654b9f23a55784c78bd34a070d8de42ff
jsep <jomchai.suk09@gmail.com>
2025-01-25 10:22:44 +0700

    Add studentCreate page

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,14 +1,19 @@
-import StudentsList from "./pages/StudentsList";
 import { useRoutes } from "react-router-dom";
 import "./App.css";
+import StudentsList from "./pages/StudentsList";
 import StudentView from "./pages/StudentView";
 import StudentEdit from "./pages/StudentEdit";
+import StudentCreate from "./pages/StudentCreate";
 
 function App() {
   const element = useRoutes([
     {
       path: "/",
       element: <StudentsList />,
+    },
+    {
+      path: "/create",
+      element: <StudentCreate />,
     },
     {
       path: "/view/:id",
---
+++ App/src/pages/StudentCreate.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentCreate = () => {
+  return <div>StudentCreate</div>;
+};
+
+export default StudentCreate;
