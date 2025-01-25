6fa8b1f88f3240675a8f88587c86ce78f33ca2c2
jsep <jomchai.suk09@gmail.com>
2025-01-25 10:18:11 +0700

    fix svg error

--- App/src/components/Control.tsx
+++ App/src/components/Control.tsx
@@ -25,9 +25,9 @@ const Control = () => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m1 1 4 4 4-4"
               />
             </svg>
@@ -95,9 +95,9 @@ const Control = () => {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
                 />
               </svg>
